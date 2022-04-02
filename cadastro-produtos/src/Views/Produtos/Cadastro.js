c85a39971dffc88303309a3032d6470a04fae5e6
giovannirwp <giovanni.ribeiro@betrybe.com>
2022-04-01 23:47:37 -0300

    fefactor: refatorando component de cadastro e edição

--- cadastro-produtos/src/Views/Produtos/Cadastro.js
+++ cadastro-produtos/src/Views/Produtos/Cadastro.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
+
 import produtoService from "../../app/produtoService";
 import Message from "../../Components/Message";
 import { withRouter } from "react-router-dom";
+import Card from "../../Components/Card";
 
 const estadoInicial = {
   nome: "",
@@ -73,103 +75,103 @@ class CadastroProduto extends Component {
   render() {
     const { nome, sku, descricao, preco, fornecedor } = this.state;
     return (
-      <div className="card">
-        <div className="card-header">
-          {this.state.atualizando ? "Atualização" : "Cadastro"}
-          de produto
-        </div>
-        <div className="card-body">
-          <form id="frmProduto" onSubmit={this.onSumbmit}>
-            {this.state.success && <Message />}
-            {this.state.errors.length > 0 &&
-              this.state.errors.map((msg) => {
-                return (
-                  <div className="alert alert-dismissible alert-danger">
-                    <button
-                      type="button"
-                      className="btn-close"
-                      data-bs-dismiss="alert"
-                    ></button>
-                    <strong> Erro!</strong>
-                    {msg}
-                  </div>
-                );
-              })}
-            <div className="row mt-3">
-              <div className="col-md-6">
-                <label>Nome: *</label>
-                <input
-                  type="text"
-                  name="nome"
-                  value={nome}
-                  onChange={this.handleChange}
-                  className="form-control"
-                />
-              </div>
-              <div className="col-md-6">
-                <label>Sku: *</label>
-                <input
-                  type="text"
-                  name="sku"
-                  disabled={this.state.atualizando}
-                  value={sku}
-                  onChange={this.handleChange}
-                  className="form-control"
-                />
-              </div>
+      <Card
+        header={
+          this.state.atualizando
+            ? "Atualização de Produto"
+            : "Cadastro de Produto"
+        }
+      >
+        <form id="frmProduto" onSubmit={this.onSumbmit}>
+          {this.state.success && <Message />}
+          {this.state.errors.length > 0 &&
+            this.state.errors.map((msg) => {
+              return (
+                <div className="alert alert-dismissible alert-danger">
+                  <button
+                    type="button"
+                    className="btn-close"
+                    data-bs-dismiss="alert"
+                  ></button>
+                  <strong> Erro!</strong>
+                  {msg}
+                </div>
+              );
+            })}
+          <div className="row mt-3">
+            <div className="col-md-6">
+              <label>Nome: *</label>
+              <input
+                type="text"
+                name="nome"
+                value={nome}
+                onChange={this.handleChange}
+                className="form-control"
+              />
+            </div>
+            <div className="col-md-6">
+              <label>Sku: *</label>
+              <input
+                type="text"
+                name="sku"
+                disabled={this.state.atualizando}
+                value={sku}
+                onChange={this.handleChange}
+                className="form-control"
+              />
+            </div>
+          </div>
+
+          <div className="row mt-3">
+            <div className="col-md-12">
+              <label>Descricção: *</label>
+              <textarea
+                value={descricao}
+                name="descricao"
+                onChange={this.handleChange}
+                className="form-control"
+              />
+            </div>
+          </div>
+
+          <div className="row mt-3 mb-3">
+            <div className="col-md-6">
+              <label>Preço: *</label>
+              <input
+                value={preco}
+                name="preco"
+                type="text"
+                onChange={this.handleChange}
+                className="form-control"
+              />
+            </div>
+            <div className="col-md-6">
+              <label>Fornecedor: *</label>
+              <input
+                type="text"
+                name="fornecedor"
+                onChange={this.handleChange}
+                value={fornecedor}
+                className="form-control"
+              />
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-md-1">
+              <button type="submit" className="btn btn-success">
+                {this.state.atualizando ? "Atualizar" : "Salvar"}
+              </button>
             </div>
 
-            <div className="row mt-3">
-              <div className="col-md-12">
-                <label>Descricção: *</label>
-                <textarea
-                  value={descricao}
-                  name="descricao"
-                  onChange={this.handleChange}
-                  className="form-control"
-                />
-              </div>
+            <div className="col-md-1">
+              <button className="btn btn-primary" onClick={this.clearInputs}>
+                Limpar
+              </button>
             </div>
-
-            <div className="row mt-3 mb-3">
-              <div className="col-md-6">
-                <label>Preço: *</label>
-                <input
-                  value={preco}
-                  name="preco"
-                  type="text"
-                  onChange={this.handleChange}
-                  className="form-control"
-                />
-              </div>
-              <div className="col-md-6">
-                <label>Fornecedor: *</label>
-                <input
-                  type="text"
-                  name="fornecedor"
-                  onChange={this.handleChange}
-                  value={fornecedor}
-                  className="form-control"
-                />
-              </div>
-            </div>
-
-            <div className="row">
-              <div className="col-md-1">
-                <button type="submit" className="btn btn-success">
-                  {this.state.atualizando ? "Atualizar" : "Salvar"}
-                </button>
-              </div>
-
-              <div className="col-md-1">
-                <button className="btn btn-primary" onClick={this.clearInputs}>
-                  Limpar
-                </button>
-              </div>
-            </div>
-          </form>
-        </div>
-      </div>
+          </div>
+        </form>
+      </Card>
     );
   }
 }
--- cadastro-produtos/src/Views/Produtos/Consulta.js
+++ cadastro-produtos/src/Views/Produtos/Consulta.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import produtoService from "../../app/produtoService";
-import { withRouter } from 'react-router-dom';
+import { withRouter } from "react-router-dom";
+import Card from "../../Components/Card";
 
 class Consulta extends Component {
   state = {
@@ -18,50 +19,57 @@ class Consulta extends Component {
   }
 
   preparaEditar = (sku) => {
-    console.log('sku para editar: ',sku)
+    console.log("sku para editar: ", sku);
     this.props.history.push(`/cadastro-produtos/${sku}`);
-  }
+  };
 
   delete = (sku) => {
     const produtos = this.service.deletar(sku);
-    this.setState({produtos})
-  }
+    this.setState({ produtos });
+  };
 
   render() {
     return (
-      <div className="card">
-        <div className="card-header">Consulta Produtos</div>
-        <div className="card-body">
-          <table className="table table-hover">
-            <thead>
-              <tr>
-                <th scope="col">Nome</th>
-                <th scope="col">Sku</th>
-                <th scope="col">Preço</th>
-                <th scope="col">Fornecedor</th>
-                <th scope="col"></th>
-              </tr>
-            </thead>
+      <Card header="Consulta Produtos">
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">Nome</th>
+              <th scope="col">Sku</th>
+              <th scope="col">Preço</th>
+              <th scope="col">Fornecedor</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
 
-            <tbody>
-              {this.state.produtos.map((produto, index) => {
-                return (
-                  <tr key={index}>
-                    <td>{produto.nome}</td>
-                    <td>{produto.sku}</td>
-                    <td>{produto.preco}</td>
-                    <td>{produto.fornecedor}</td>
-                    <td>
-                      <button onClick={ () => this.preparaEditar(produto.sku)} className="btn btn-primary">Editar</button>
-                      <button onClick={ () => this.delete(produto.sku)} className="btn btn-danger">Remover</button>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
-      </div>
+          <tbody>
+            {this.state.produtos.map((produto, index) => {
+              return (
+                <tr key={index}>
+                  <td>{produto.nome}</td>
+                  <td>{produto.sku}</td>
+                  <td>{produto.preco}</td>
+                  <td>{produto.fornecedor}</td>
+                  <td>
+                    <button
+                      onClick={() => this.preparaEditar(produto.sku)}
+                      className="btn btn-primary"
+                    >
+                      Editar
+                    </button>
+                    <button
+                      onClick={() => this.delete(produto.sku)}
+                      className="btn btn-danger"
+                    >
+                      Remover
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </Card>
     );
   }
 }
